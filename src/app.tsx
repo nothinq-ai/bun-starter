fad1432928d8da2c3350aebcafacdb648e78e5a8
Trần Bá Thanh Tùng <tranbathanhtung222@gmail.com>
2025-11-18 17:04:17 +0700

    chore: add theme provider

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { Home } from "./pages/home";
+import { ThemeProvider } from "./components/theme-provider";
 import "./globals.css";
 
 export function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
